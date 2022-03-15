cb83f6585558bf8b2e76420260548e5b02754ed2
fredrvaa <fredrvaa@stud.ntnu.no>
2022-03-15 12:25:31 +0100

    Implement order crossover

--- evolution/offspring.jl
+++ evolution/offspring.jl
@@ -1,15 +1,47 @@
 using IterTools
 
-function crossover!(population, probability=0.7, n_points=1)
-    for (o1, o2) in partition(eachrow(population), 2)
-        if rand() < probability
-            for n in 1:n_points
-                p = rand(1:(length(o1) - 1))
-                temp1 = copy(o1[p:end])
-                temp2 = copy(o2[p:end])
-                o2[p:end] = temp1
-                o1[p:end] = temp2
+function crossover!(population, probability=0.7)
+    """
+    Order crossover
+    
+    Example:
+
+    5 patients, 3 nurses
+    ---------------------
+    1 2 -1 | 3 -2 4 | 5 (p1)
+    4 -2 2 | -1 1 3 | 5 (p2)
+    ========================
+    2 -1 1 | 3 -2 4 | 5 (o1)
+    2 -2 4 | -1 1 3 | 5 (o2)
+    """
+
+    function create_offspring(parent1, parent2, chromosome_length)
+        point1 = rand(1:(chromosome_length - 1))
+        point2 = rand((point1 + 1):chromosome_length)
+        rightshift = chromosome_length - point2
+        offspring = zeros(Int64, chromosome_length)
+        offspring[point1:point2] = parent1[point1:point2]
+        offspring = circshift(offspring, rightshift)
+        i = 1
+        for c in circshift(parent2, chromosome_length - point2)
+            if !(c in offspring) 
+                offspring[i] = c
+                i += 1
             end
+            if i == point1 + rightshift
+                break
+            end
+        end
+        return circshift(offspring, point2)
+    end
+
+    chromosome_length = length(population[1, :])
+    for (parent1, parent2) in partition(eachrow(population), 2)
+        if rand() < probability
+            offspring1 = create_offspring(parent1, parent2, chromosome_length)
+            offspring2 = create_offspring(parent2, parent1, chromosome_length)
+            parent1 = offspring1
+            parent2 = offspring2
         end
     end
     return population
