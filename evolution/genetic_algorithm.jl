98767cc1e85d98a49c13e3dd24af3d7921cdc48e
Fredrik <fredrvaa@stud.ntnu.no>
2022-03-18 17:26:37 +0100

    Fix bug where max traveldistance was visualized

--- evolution/genetic_algorithm.jl
+++ evolution/genetic_algorithm.jl
@@ -3,6 +3,7 @@ include("fitness.jl")
 include("parent_selection.jl")
 include("survivor_selection.jl")
 include("offspring.jl")
+include("feasibility.jl")
 include("../utils/visualize.jl")
 
 function genetic_algorithm(instance, population_size=100, generations=100, elitism_frac=0.1, p_crossover=0.9, p_mutate=0.01)
@@ -10,6 +11,7 @@ function genetic_algorithm(instance, population_size=100, generations=100, eliti
 
     # Initialize population
     population = initialize(population_size, instance)
+    println(map(x -> isfeasible(x, instance), eachrow(population)))
     # GA loop
     for n in 1:generations
         # Calculate and record fitness
@@ -23,7 +25,7 @@ function genetic_algorithm(instance, population_size=100, generations=100, eliti
         crossover!(population, p_crossover)
         mutate!(population, p_mutate)
         
-        best_fit = population[argmax(fitness), :]
+        best_fit = population[argmin(fitness), :]
         visualize(best_fit, instance, fitness_history)
     end
 
