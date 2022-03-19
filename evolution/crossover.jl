f95c2496cb37f4a80310c36753044c6254d72b82
Fredrik <fredrvaa@stud.ntnu.no>
2022-03-19 13:39:17 +0100

    Implement position crossover

--- evolution/crossover.jl
+++ evolution/crossover.jl
@@ -1,7 +1,7 @@
 using IterTools
 using Random
 
-function crossover!(population, probability=0.7)
+function order(parent1, parent2)
     """
     Order crossover
     
@@ -15,10 +15,7 @@ function crossover!(population, probability=0.7)
     2 -1 1 | 3 -2 4 | 5 (o1)
     2 -2 4 | -1 1 3 | 5 (o2)
     """
-
-    function create_offspring(parent1, parent2, chromosome_length)
-        point1 = rand(1:(chromosome_length - 1))
-        point2 = rand((point1 + 1):chromosome_length)
+    function create_offspring(parent1, parent2, point1, point2, chromosome_length)
         rightshift = chromosome_length - point2
         offspring = zeros(Int64, chromosome_length)
         offspring[point1:point2] = parent1[point1:point2]
@@ -36,11 +33,35 @@ function crossover!(population, probability=0.7)
         return circshift(offspring, point2)
     end
 
-    chromosome_length = length(population[1, :])
+    chromosome_length = length(parent1)
+    point1 = rand(1:(chromosome_length - 1))
+    point2 = rand((point1 + 1):chromosome_length)
+
+    offspring1 = create_offspring(parent1, parent2, point1, point2, chromosome_length)
+    offspring2 = create_offspring(parent2, parent1, point1, point2, chromosome_length)
+    return offspring1, offspring2
+end
+
+function position(parent1, parent2)
+    offspring1 = copy(parent1)
+    offspring2 = copy(parent2)
+    for (i, (node1, node2)) in enumerate(zip(offspring1, offspring2))
+        if rand() < 0.6 && node1 > 0 && node2 > 0
+            offspring1[i] = node2
+            offspring2[i] = node1
+        end
+    end
+    return offspring1, offspring2
+end
+
+function crossover!(population, probability)
     for (parent1, parent2) in partition(eachrow(population), 2)
         if rand() < probability
-            offspring1 = create_offspring(parent1, parent2, chromosome_length)
-            offspring2 = create_offspring(parent2, parent1, chromosome_length)
+            if rand() < 0.5
+                offspring1, offspring2 = order(parent1, parent2)
+            else
+                offspring1, offspring2 = position(parent1, parent2)
+            end
             parent1 = offspring1
             parent2 = offspring2
         end
