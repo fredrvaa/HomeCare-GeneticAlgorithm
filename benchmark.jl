93b08c61939ed3d6f694ebdf45c6911564e35b81
Fredrik <fredrvaa@stud.ntnu.no>
2022-03-21 18:27:11 +0100

    Update benchmark script

--- benchmark.jl
+++ benchmark.jl
@@ -10,10 +10,13 @@ include("utils/output.jl")
 for i in 0:9
     instance = get_instance("train/train_$i.json")
 
-    individual = genetic_algorithm(instance, false, 100, 3000, 0.1, 0.95, 0.98, 1, 0, 6)
+    individual = genetic_algorithm(instance, false, 150, 3000, 0.05, 0.95, 0.98, 1, 0, 6, 0.01, 25)
+    fitness = individual_fitness(individual, instance)
 
     open("benchmarks/benchmark_$i.txt", "w") do io
-        println(io, individual_fitness(individual, instance))
+        fitness_text = @sprintf("Fitness %.2f", fitness)
+        instance_text = @sprintf("%s %.2f%% %.2f", instance[:instance_name], 100 * (1 - instance[:benchmark] / fitness), instance[:benchmark])
+        println(io, "$fitness_text | $instance_text")
         print_list(io, tolist(individual))
     end
 end
