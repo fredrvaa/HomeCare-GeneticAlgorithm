3a6579aee6e50bc2784790643582992b92cf9daf
Fredrik <fredrvaa@stud.ntnu.no>
2022-03-21 18:25:58 +0100

    Implement edge crossover

--- evolution/crossover.jl
+++ evolution/crossover.jl
@@ -42,25 +42,64 @@ function order(parent1, parent2)
     return offspring1, offspring2
 end
 
-function position(parent1, parent2)
-    offspring1 = copy(parent1)
-    offspring2 = copy(parent2)
-    for (i, (node1, node2)) in enumerate(zip(offspring1, offspring2))
-        if rand() < 0.6 && node1 > 0 && node2 > 0
-            offspring1[i] = node2
-            offspring2[i] = node1
+function edge_crossover(parent1, parent2)
+    function create_offspring!(neighbours, chromosome_length)
+        offspring = zeros(Int64, chromosome_length)
+        node = rand(keys(neighbours))
+        delete!(neighbours, node)
+        for (k, v) in neighbours
+            neighbours[k] = delete!(v, node)
         end
+        offspring[1] = node
+
+        for i in 2:chromosome_length            
+            min_length = Inf
+            node = 0
+            for (k, v) in neighbours
+                len = length(v)
+                if len < min_length
+                    min_length = len
+                    node = k
+                elseif len == min_length && rand() < 0.5
+                    node = k
+                end
+            end
+            delete!(neighbours, node)
+            for (k, v) in neighbours
+                delete!(v, node)
+            end
+            offspring[i] = node
+        end
+        return offspring
     end
+
+    chromosome_length = length(parent1)
+
+    # Construct neighbour lists
+    neighbours1 = Dict()
+    for node in parent1
+        neighbours1[node] = Set([])
+    end
+    for i in 1:chromosome_length
+        n1 = i > 1 ? i - 1 : chromosome_length
+        n2 = i < chromosome_length ? i + 1 : 1
+        push!(neighbours1[parent1[i]], parent1[n1], parent1[n2])
+        push!(neighbours1[parent2[i]], parent2[n1], parent2[n2])
+    end
+    neighbours2 = deepcopy(neighbours1)
+    offspring1 = create_offspring!(neighbours1, chromosome_length)
+    offspring2 = create_offspring!(neighbours2, chromosome_length)
+
     return offspring1, offspring2
 end
 
 function crossover!(population, probability=0.7)
     for (i, (parent1, parent2)) in enumerate(partition(eachrow(population), 2))
         if rand() < probability
-            if rand() < 1
+            if rand() < 0.5
                 offspring1, offspring2 = order(parent1, parent2)
             else
-                offspring1, offspring2 = position(parent1, parent2)
+                offspring1, offspring2 = edge_crossover(parent1, parent2)
             end
             population[2i-1, :] = offspring1
             population[2i, :] = offspring2
